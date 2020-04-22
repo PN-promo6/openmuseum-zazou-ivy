a57584e39608cc51648c225cec1a9d94b876a6f4
lea zaoui <lea.myriam.zaoui@gmail.com>
2020-04-22 09:42:01 +0200

    Adding new feature of Openmuseum

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import { AppComponent } from './app.component';
-// Service contenant le JSON de tous les musées et les méthodes associées
 import { MuseumListService } from './services/museumList.service';
-// Liste des musée : inclue SingleMuseumComponent
-import { MuseumListComponent } from './museum-list/museum-list.component';
-// Un musée dans la liste
+import { AppComponent } from './app.component';
 import { SingleMuseumComponent } from './single-museum/single-museum.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AddComponent } from './add/add.component';
+import { IndexComponent } from './index/index.component';
+import { MuseumDetailComponent } from './museum-detail/museum-detail.component';
 
-const appRoutes:Routes = [
-  {path: '', component: MuseumListComponent}
+
+
+const appRoutes: Routes = [
+  { path: 'add', component : AddComponent }, //To add a museum
+  { path: 'museumDetail', component: MuseumDetailComponent },
+  { path: '', component: IndexComponent } //To create a home page => It's normal It is empty, there's nothing left in
+
 ]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SingleMuseumComponent,
-    MuseumListComponent,
+    IndexComponent,
+    AddComponent,
+    MuseumDetailComponent
+
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+      RouterModule.forRoot(appRoutes)
   ],
   providers: [MuseumListService],
   bootstrap: [AppComponent]
